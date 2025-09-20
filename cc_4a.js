45c859b80c3cc180eb60924129c7515bf07762ad
Jawad Sweilem <jsweilem@usf.edu>
2025-09-20 14:35:19 -0400

    deleted everything

--- cc_4a.js
+++ cc_4a.js
@@ -1,14 +1,4 @@
-let customers = [
-{name: "John Smith", email: "js1@gmail.com", purchases: "Sock, Shoe, Shirt"},
-{name: "Roonil Wazlib", email: "rw2@gmail.com", purchases: "Broom, Helmet, Robe"},
-{name: "Jayden Smith", email: "js2@gmail.com", purchases: "IPhone, Jeans, TV"},
-]
-customers.forEach(obj => console.log(`Name: ${obj.name}`));
-customers.forEach(obj => console.log(obj));
+console.log("Hello from JavaScript!");
 
-customers.forEach(customer => {
-  console.log(`Name: ${customer.name}`);
-  console.log(`Email: ${customer.email}`);
-  console.log(`Purchases: ${customer.purchases}`);
-  console.log("----------");
-});
+let message = "JS test successful";
+console.log(message);
